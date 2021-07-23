455c111d3356eb851c46e418b820857aefa218da
shive <shive@DESKTOP-H7QUKCV>
2021-07-23 15:00:29 +0530

    master push

---
+++ src/Fruit_220.java
@@ -0,0 +1,17 @@
+
+public class Fruit_220 {
+	
+	String color;
+	
+	Fruit_220(String color)
+	{
+		this.color = color;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
